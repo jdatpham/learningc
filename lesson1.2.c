a64efef591ab4a524016900ced665c3199d7d3ae
Jason Pham <jdatpham@gmail.com>
2021-01-03 21:36:32 -0600

    lesson 1.2

---
+++ lesson1.2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+  // includeing the start
+int main(void){
+    //intger main void
+    int i;
+
+    //printf(" I love love love programing \n ");
+
+    printf("I ");
+
+    for(i = 0; i < 3 ; i++) {
+        printf("love ");
+    }
+    printf("programming!\n");
+    
+    // printing out the love love love coding part
+    
+    return 0;
+} 
