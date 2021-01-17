6380b807038a3aa9ad32bb37af933b2e8b3a545e
Jason Pham <jdatpham@gmail.com>
2021-01-17 13:52:41 -0600

    added comment

--- temperature_f2c.c
+++ temperature_f2c.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 int main(void) {
     double temp_c;
     double temp_f;
-    
+
+    // Get the temperature input in Celcius
     scanf("%lf", &temp_c);
+    // Convert Celcisus to Fahrenheit
     temp_f = temp_c * 9.0 / 5.0 + 32.0;
     printf("%.1lf\n", temp_f);
         
