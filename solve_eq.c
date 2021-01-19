f9e8f9f1174022a82e35fba1a4fd2d15c6ed09be
Jason Pham <jdatpham@gmail.com>
2021-01-18 19:59:32 -0600

    has the program do an equation

---
+++ solve_eq.c
@@ -0,0 +1,28 @@
+# include<stdio.h>
+
+int main(void) {
+    float a;
+    float b;
+    float c;
+    printf("Enter first parameter: \n");
+    scanf("%f", &a);
+
+    printf("Enter first parameter: \n");
+    scanf("%f", &b);
+
+    printf("Enter first parameter: \n");
+    scanf("%f", &c);
+
+    // ax + b = c
+    printf("Solve the equation: %.2fx + %.2f = %.2f\n", a, b, c);
+    // ax + b - b = c - b
+    printf(" <=> %.2fx + %.2f - %.2f = %.2f - %.2f\n", a, b, b, c, b);
+    // ax = c - b
+    printf(" <=> %.2fx = %.2f - %.2f\n", a, c, b);
+    // x = (c-b)/a
+    printf(" <=> x = (%g - %.2f) / %.2f = %.2f/%.2f  \n", c, b, a, c-b, a);
+    printf(" <=> x = %.2f\n", (c - b)/a);
+
+
+    return 0 ;
+}
