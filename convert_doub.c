b7d6f26d58f92bb32c2b98c1e2e273c893869897
Jason Pham <jdatpham@gmail.com>
2021-01-21 20:27:12 -0600

    finds projection of growth rate

---
+++ convert_doub.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+    double grow_rate;
+    int current_population;
+    int an;
+
+    scanf("%d", &current_population);
+    scanf("%lf", &grow_rate);
+    an =  current_population * (1.0 + (grow_rate/100.0));
+    printf("%d\n", an);
+    return 0;
+}
