3227cac051391e497c1a41bc94ffa9b86f8afa52
Jason Pham <jdatpham@gmail.com>
2021-02-06 22:06:19 -0600

    finds hotel cost based on time arriving

---
+++ unit1_getting_started/in1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+   
+    int a;
+    int b;
+    int result;
+    printf("Check: Is a == b?\n");
+    result = a==b;
+    return 0;
+}
---
+++ unit2_language_foundations/hostel.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    int hour;
+    int cost;
+
+    scanf("%d", &hour);
+    if(hour < 0 || hour > 12){
+        printf("Invalid Input. Must be between 0 and 12\n");
+        return -1;
+    }
+    cost = 10 + hour * 5;
+
+    if(cost>53){
+        cost = 53;
+    }
+    
+    printf("%d\n", cost);
+
+    return 0;
+}
