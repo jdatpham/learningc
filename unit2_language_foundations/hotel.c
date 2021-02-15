814c454aa90e54011bab87bf8aa516bac5840b29
Jason Pham <jdatpham@gmail.com>
2021-02-14 21:10:33 -0600

    change variable from room_price to price

--- unit2_language_foundations/hotel.c
+++ unit2_language_foundations/hotel.c
@@ -3,17 +3,17 @@
 int main(void) {
     int age = 0;
     int luggage = 0;
-    int room_price = 0;
+    int price = 0;
     int luggage_cost = 0;
 
 
     scanf("%d", &age);
     scanf("%d", &luggage);
-    if (age >= 60){
-        room_price = 0;
+    if (age == 60){
+        price = 0;
     }
     else if (age < 10){
-        room_price = 5;
+        price = 5;
     } 
     else {
 
@@ -21,11 +21,11 @@ int main(void) {
         if(luggage > 20){
             luggage_cost =  10;
         }
-        room_price = 30 + luggage_cost;
+        price = 30 + luggage_cost;
     }
     
     
-    printf("%d\n", room_price);
+    printf("%d\n", price);
 
     return 0;
 }
